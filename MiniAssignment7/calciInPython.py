d4a1f27c56f8efe606f6484239fee5cc6ca97c65
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-04-06 20:34:05 +0530

    updated calci problem

--- MiniAssignment7/calciInPython.py
+++ MiniAssignment7/calciInPython.py
@@ -1,3 +1,21 @@
-user_input=input("enter the String: ")
+# print(user_input.split())
+import sys
 
-print(user_input)
+while (True):
+    user_input = input("enter the String: ")
+    if (user_input == "quit"):
+        sys.exit()
+    ls1 = list(user_input.split(" "))
+    try:
+        num1 = float(ls1[0])
+        num2 = float(ls1[2])
+        if (ls1[1] == "+"):
+            print(num2 + num1)
+        elif (ls1[1] == "-"):
+            print(num1 - num2)
+        else:
+            raise Exception()
+    except Exception as e:
+        print("!!Incorrect format ,please enter correct Formulae")
+    finally:
+        print("bye")
