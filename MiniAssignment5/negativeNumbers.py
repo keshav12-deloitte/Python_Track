444317b9f1e2a25e607403da57c2baaa34511bcb
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-04-06 13:23:43 +0530

    updated neagtiveNumbersProblems

--- MiniAssignment5/negativeNumbers.py
+++ MiniAssignment5/negativeNumbers.py
@@ -9,3 +9,8 @@ lst2=list(filter(negative,lst1))
 print(lst2)
 lst2=list(map(lambda x:x-2*x,lst2))
 print(lst2)
+
+#using map in filter
+lst3=[-1000, 500, -600, 700, 5000, -90000, -17500]
+lst4=list(map(lambda x:abs(x),(filter(lambda x:x<0,map(lambda x:x,lst3)))))
+print(lst4)
