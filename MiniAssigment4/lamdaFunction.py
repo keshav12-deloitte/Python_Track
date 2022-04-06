5c8724a56af5c7891c6f02ea56b75b278221ec70
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-04-06 11:35:15 +0530

    Updated problem 1 with lamda and map

--- MiniAssigment4/lamdaFunction.py
+++ MiniAssigment4/lamdaFunction.py
@@ -1,21 +1,17 @@
 
-"""
+#using only lamda
 a=int(input("enter the value of a : "))
 b=int(input("enter the value of b : "))
 c=int(input("enter the value of c : "))
 x=int(input("enter the value of x : "))
 
-equation=[]
-equation.append(a)
-equation.append(b)
-equation.append(c)
-equation.append(x)
-print(equation)
-root=list(map(lambda a,b,c,x:a(x*x)+b(x)+c,))
-print(root)
-"""
 
-output = lambda a, b, c, x: (a*x*x)+(b*x)+c
-y = output(*map(int,input("enter numbers: ").split()))
+output =lambda a, b, c, x: a * x * x + b * x + c
+print(output(a,b,c,x))
+
+
+#using lamda and map
+output1=lambda a,b,c,x :a * x * x + b * x + c
+y=output1(*map(int,input("enter the value").split()))
 print(y)
 
