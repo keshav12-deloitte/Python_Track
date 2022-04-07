351b5282f7d6a31cdaa08c3c580becbc063c6fe5
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-04-07 22:11:20 +0530

    resloved conflicts in admin

--- MainAssignmnet/BookMyShow.py
+++ MainAssignmnet/BookMyShow.py
@@ -15,18 +15,17 @@ class AdminOfBookMyShow():
     def __init__(self, adminName, adminPassword):
         self.adminName = adminName
         self.adminPassword = adminPassword
-        print("******WelcomeAdmin*******\n1.Add New Movie Info\n2.Edit Movie\n3.Delete Movie\n4.Logout")
 
-    def adminOperations(self, choice):
+
+    def adminOperations(self):
         while (True):
+            print("******WelcomeAdmin*******\n1.Add New Movie Info\n2.Edit Movie\n3.Delete Movie\n4.Logout")
+            choice=int(input("Enter the choice you want to perform"))
             row, col, sh1, wb = workbook("MoviesInfo.xlsx", "Sheet1")
+            if(choice==4):
+                break
             if (choice == 1):
-                # wb = openpyxl.load_workbook("MoviesInfo.xlsx")
-                # sh1 = wb['Sheet1']
-                # row = sh1.max_row
-                # col = sh1.max_column
-                # print(row, col)
-                print("enter the details of the movie")
+                print("enter the details of the movie: ")
                 row1 = 1
                 column1 = 1
                 for c in range(1, col + 1):
@@ -34,40 +33,27 @@ class AdminOfBookMyShow():
                     sh1.cell(row + 1, c, value=input(f"Enter the {m}: "))
                     column1 += 1
                 wb.save("updated.xlsx")
-                break
-            elif (choice == 2):
+                # break
+            elif(choice == 2):
                 movieNameToBeEdited = input("enter the movie name that you want to edit: ")
-                # wb = openpyxl.load_workbook("MoviesInfo.xlsx")
-                # sh1 = wb['Sheet1']
-                # row = sh1.max_row
-                # col = sh1.max_column
-                # column1 = 1
-                # newName= sh1.cell(row=1, column=1).value
                 for i in range(2, row + 1):
                     name = sh1.cell(i, column=1).value
                     if (movieNameToBeEdited == name):
                         for j in range(1, col + 1):
-                            for m in range(1, col + 1):
-                                newName = sh1.cell(1, m).value
-                                sh1.cell(i, j, value=input(f"Enter the {newName} to change: "))
+                                sh1.cell(i, j, value=input(f"Enter the {sh1.cell(1, j).value} new change: "))
                         # column1 += 1
                 wb.save("MoviesInfo.xlsx")
-                break
             elif (choice == 3):
                 movieToBeDeleted = input("Enter the movie name you want to Delete: ")
-                # wb = openpyxl.load_workbook("MoviesInfo.xlsx")
-                # sh1 = wb['Sheet1']
-                # row = sh1.max_row
-                # col = sh1.max_column
                 for i in range(2, row + 1):
                     name = sh1.cell(i, column=1).value
                     if (name == movieToBeDeleted):
                         sh1.delete_rows(i, True)
-                wb.save("updated2.xlsx")
-                break
-            elif (choice == 4):
-                print("You have Successfully Logged out")
-                sys.exit()
+                wb.save("MoviesInfo.xlsx")
+                #break
+            # elif (choice == 4):
+            #     print("You have Successfully Logged out")
+            #     sys.exit()
 
 
 class UserDetails(AdminOfBookMyShow):
@@ -94,29 +80,33 @@ class UserDetails(AdminOfBookMyShow):
         for i in range(1, col + 1):
             print(sh1.cell(1, i).value, end=": ")
             print(sh1.cell(choiceByUser + 1, i).value)
-        print("1.Book Tickets\n2. Cancel Ticket \n3. Give User Rating ")
-        userTicketChoice = int(input("Enter the choice from above: "))
-        if (userTicketChoice == 1):
-            print("***** Welcome User *****")
-            for i in range(7, 9):
-                print(f"Timing : {sh1.cell(choiceByUser + 1, i).value}")
-            timing = int(input("enter the timing you want to choose: "))
-            print(f"selected timing {timing}")
-            print(f"Remaining Seats is {sh1.cell(choiceByUser + 1, 13).value}")
-            totalTicketsToBeBooked = int(input("Enter the number of tickets to be Booked:"))
-            ticketsAvailable = sh1.cell(choiceByUser + 1, 13).value
-            sh1.cell(choiceByUser + 1, 13, value=ticketsAvailable - totalTicketsToBeBooked)
-            print(sh1.cell(choiceByUser + 1, 13).value)
-            print("***** Thank You for Booking Tickets *****")
-            wb.save("MoviesInfo.xlsx")
-        elif (userTicketChoice == 2):
-            ticketsTobeCancelled = int(input("enter the Number of tickets to be Cancelled"))
-            ticketsAvailable = sh1.cell(choiceByUser + 1, 13).value
-            sh1.cell(choiceByUser + 1, 13, value=ticketsAvailable + ticketsTobeCancelled)
-            print(sh1.cell(choiceByUser + 1, 13).value)
-            wb.save("MoviesInfo.xlsx")
-        elif (userTicketChoice == 3):
-            userRating = int(input("Enter your Review for above movie"))
+        while(True):
+            print("1.Book Tickets\n2. Cancel Ticket \n3. Give User Rating \n4.exit")
+            userTicketChoice = int(input("Enter the choice from above: "))
+            if (userTicketChoice == 1):
+                print("***** Welcome User *****")
+                for i in range(7, 9):
+                    print(f"Timing : {sh1.cell(choiceByUser + 1, i).value}")
+                timing = int(input("enter the timing you want to choose: "))
+                print(f"selected timing {timing}")
+                print(f"Remaining Seats is {sh1.cell(choiceByUser + 1, 13).value}")
+                totalTicketsToBeBooked = int(input("Enter the number of tickets to be Booked:"))
+                ticketsAvailable = sh1.cell(choiceByUser + 1, 13).value
+                sh1.cell(choiceByUser + 1, 13, value=ticketsAvailable - totalTicketsToBeBooked)
+                print(sh1.cell(choiceByUser + 1, 13).value)
+                print("***** Thank You for Booking Tickets *****")
+                wb.save("MoviesInfo.xlsx")
+            elif (userTicketChoice == 2):
+                ticketsTobeCancelled = int(input("enter the Number of tickets to be Cancelled"))
+                ticketsAvailable = sh1.cell(choiceByUser + 1, 13).value
+                sh1.cell(choiceByUser + 1, 13, value=ticketsAvailable + ticketsTobeCancelled)
+                print(sh1.cell(choiceByUser + 1, 13).value)
+                wb.save("MoviesInfo.xlsx")
+            elif (userTicketChoice == 3):
+                userRating = input("Enter your Review for above movie")
+            elif(userTicketChoice==4):
+                break
+
 
     def userActions(self):
         row, col, sh1, wb = workbook("MoviesInfo.xlsx", "Sheet1")
@@ -125,6 +115,7 @@ class UserDetails(AdminOfBookMyShow):
             print(m, end='.')
             print(sh1.cell(i, 1).value)
             m += 1
+        print("4.Logout")
 
         choiceByUser = int(input("Enter the choice you want to choose"))
         if (choiceByUser == 1):
@@ -140,25 +131,30 @@ class UserDetails(AdminOfBookMyShow):
 
 
 
-print("******Welcome to BookMyShow*******\n1.Login\n2.Register new account\n3.Exit")
-user_input = int(input("Enter your choice: "))
+
+
 while (True):
-    if (user_input == 1):
-        print("Welcome\n1.AdminLogin\n2.UserLogin")
+    print("******Welcome to BookMyShow*******\n1.Login\n2.Register new account\n3.Exit")
+    user_input = int(input("Enter your choice: "))
+    if(user_input==3):
+        break
+    elif (user_input == 1):
+        print("Welcome\n1.AdminLogin\n2.UserLogin\n3.Exit")
         loginType = int(input("enter your choice"))
         if (loginType == 1):
             name = input("enter you name: ")
             password = input("enter your password: ")
             admin = AdminOfBookMyShow(name, password)
-            admin.adminOperations(int(input("Enter your choice to perform: ")))
-        if (loginType == 2):
+            admin.adminOperations()
+        elif (loginType == 2):
             userName = input("enter your UserName: ")
             UserPassword = input("enter the password: ")
             keshav = UserDetails(userName, UserPassword)
             keshav.userRegistration(userName, UserPassword)
             keshav.userActions()
 
-        break
+
+        #break
     elif (user_input == 2):
         userName = input("Enter Your Name")
         # userEmail = input("Enter Your Email")
@@ -170,8 +166,6 @@ while (True):
         user_Password = input("Enter your Password")
         keshav.userRegistration(userName, user_Password)
         keshav.userActions()
-
-
-        break
+        # break
     else:
         sys.exit()
